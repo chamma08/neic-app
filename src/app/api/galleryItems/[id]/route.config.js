c14822f879cc44d5bf2b2c131ee8c8793ca13c80
Matheesha Dissanayake <SA22524904@my.sliit.lk>
2025-03-04 19:32:19 +0530

    updated

--- src/app/api/galleryItems/[id]/route.config.js
+++ src/app/api/galleryItems/[id]/route.config.js
@@ -1,3 +1,3 @@
 export const runtime = "edge"; // Use edge runtime for better performance
 export const maxDuration = 60; // Increase execution time if needed
-export const bodyParser = { sizeLimit: "100mb" }; // Increase upload limit
+export const bodyParser = { sizeLimit: "50mb" }; // Increase upload limit
